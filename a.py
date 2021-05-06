0bf3945aefe6e646d2db0793f5f26dd232bf3430
Qavamodin Soleimani <soleimani.qavamodin.9821160021@gmail.com>
2021-05-06 11:45:16 +0430

    first

---
+++ a.py
@@ -0,0 +1,8 @@
+import docutils.core
+  
+docutils.core.publish_file(
+    source_path='C:\\Users\\QavamS\\Downloads\\Compressed\\rst\\example\\pos.rst',
+    destination_path='C:\\Users\\QavamS\\Downloads\\Compressed\\rst\\example\\pos.html',
+    writer_name ="html",
+    settings_overrides={'input_encoding': 'UTF-8', 'output_encoding': 'UTF-8'}
+    )
